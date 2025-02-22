c3df51bfd1300bef9593fceb593157bd08fd86cd
Christian Lønaas <christian.lonaas@ugle-z.no>
2025-02-22 16:26:29 +0100

    Remove print statements

--- flusher.go
+++ flusher.go
@@ -2,7 +2,6 @@ package slogseq
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"strings"
 	"time"
@@ -38,7 +37,6 @@ func (h *SeqHandler) runBackgroundFlusher() {
 			}
 
 		case <-h.state.doneCh:
-			fmt.Printf("doneCh closed - flushing %d events\n", len(events))
 			if len(events) > 0 {
 				h.sendBatch(events)
 			}
@@ -66,15 +64,12 @@ func (h *SeqHandler) sendBatch(events []CLEFEvent) {
 			topLevel[k] = v
 		}
 		if err := encoder.Encode(topLevel); err != nil {
-			fmt.Println(err.Error())
 			// handle error
 		}
 	}
 
-	fmt.Println("Event " + sb.String())
 	req, err := http.NewRequest("POST", h.seqURL, strings.NewReader(sb.String()))
 	if err != nil {
-		fmt.Println(err.Error())
 		// handle error
 		return
 	}
@@ -85,14 +80,12 @@ func (h *SeqHandler) sendBatch(events []CLEFEvent) {
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		fmt.Println(err.Error())
 		// handle error, maybe retry or drop
 		return
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode < 200 || resp.StatusCode > 299 {
-		fmt.Printf("unexpected status code: %d", resp.StatusCode)
 		// handle non-2xx response
 	}
 }
