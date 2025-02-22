30e5fccf90111668da8e927852accf92813579b8
Christian Lønaas <christian.lonaas@ugle-z.no>
2025-02-22 18:20:35 +0100

    Add comment

--- event.go
+++ event.go
@@ -2,6 +2,8 @@ package slogseq
 
 import "time"
 
+// Compact Log Event Format (CLEF) is a JSON-based log event format that Seq uses.
+// https://clef-json.org
 type CLEFEvent struct {
 	Timestamp  time.Time              `json:"@t"`
 	Message    string                 `json:"@m,omitempty"`
