4dca09362fc7f3c38ab984d36ed1f2ed47cba024
Christian Lønaas <christian.lonaas@ugle-z.no>
2025-02-22 16:22:44 +0100

    Test tool

---
+++ cmd/seqtest/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+	"log/slog"
+	"time"
+
+	slogseq "github.com/sokkalf/slog-seq" // import your library
+)
+
+var (
+    seqURL  = flag.String("url", "http://localhost:5341/ingest/clef", "Seq ingestion URL")
+    apiKey  = flag.String("key", "", "Seq API key")
+)
+
+func main() {
+	flag.Parse()
+	if flag.NFlag() == 0 {
+		flag.PrintDefaults()
+		return
+	}
+    handler := slogseq.NewSeqHandler(
+		*seqURL,           // seqURL
+		*apiKey,           // apiKey
+        50,               // batchSize
+        2*time.Second,    // flushInterval
+    )
+	defer handler.Close()
+
+    logger := slog.New(handler).With("app", "slog-seq").With("env", "dev").With("version", "1.0.0")
+
+    logger.Info("Hello from slog-seq test command!",
+        "env", "dev",
+        "version", "1.0.0")
+
+	logger.Warn("This is a warning message", "huba", "fjall")
+
+	logger.Error("This is an error message", "huba", "fjall")
+
+	logger.Debug("This is a debug message", "huba", "fjall")
+}
