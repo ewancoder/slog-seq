f8a999b46804d18b9aca379f67b3cfeb87018128
Christian Lønaas <christian.lonaas@ugle-z.no>
2025-03-01 15:56:09 +0100

    Remove deprecated function

--- slogseq.go
+++ slogseq.go
@@ -100,27 +100,3 @@ func WithWorkers(count int) SeqOption {
 		return h
 	})
 }
-
-// NewSeqLogger creates a new Seq logger.
-// seqURL is the URL of the Seq server.
-// apiKey is the API key for the Seq server.
-// batchSize is the number of events to batch before sending to Seq.
-// flushInterval is the interval at which to flush the batch.
-// opts is the handler options.
-// Returns the logger and a function to close the logger.
-//
-// Example:
-// 	seqLogger, finisher := slogseq.NewSeqLogger( ... )
-// 	defer finisher()
-// 	slog.SetDefault(seqLogger)
-// 	slog.Info("Hello from slog-seq!")
-
-// Deprecated: Use NewLogger instead.
-func NewSeqLogger(seqURL, apiKey string, batchSize int, flushInterval time.Duration, opts *slog.HandlerOptions) (*slog.Logger, *SeqHandler) {
-	return NewLogger(seqURL,
-		WithAPIKey(apiKey),
-		WithBatchSize(batchSize),
-		WithFlushInterval(flushInterval),
-		WithHandlerOptions(opts),
-	)
-}
